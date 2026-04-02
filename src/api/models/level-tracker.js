f859a10879c37d80d1cf0afc1354e49fe6786db6
Ediin <uai.coins@gmail.com>
2026-04-02 12:46:52 -0300

    Add ensureLevelTracker and setLevelTrackerLevel functions

--- src/api/models/level-tracker.js
+++ src/api/models/level-tracker.js
@@ -24,6 +24,43 @@ export const getLevelTrackerByName = async (name) => (
   })
 );
 
+export const ensureLevelTracker = async ({ name, level }) => (
+  new Promise(async (resolve, reject) => {
+    try {
+      let tracker = await LevelTracker.findOne({ name });
+
+      if (!tracker) {
+        tracker = new LevelTracker({
+          name,
+          lastLevel: Number(level),
+        });
+
+        await tracker.save();
+      }
+
+      resolve(tracker);
+    } catch (error) {
+      reject(error);
+    }
+  })
+);
+
+export const setLevelTrackerLevel = async ({ name, level }) => (
+  new Promise(async (resolve, reject) => {
+    try {
+      const tracker = await LevelTracker.findOneAndUpdate(
+        { name },
+        { $set: { lastLevel: Number(level) } },
+        { new: true, upsert: true }
+      );
+
+      resolve(tracker);
+    } catch (error) {
+      reject(error);
+    }
+  })
+);
+
 export const upsertLevelTracker = async ({ name, level }) => (
   new Promise(async (resolve, reject) => {
     try {
