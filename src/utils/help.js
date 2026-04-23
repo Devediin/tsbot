58bf2cb9a56832e722068aeb2a04fcc541366157
Ediin <uai.coins@gmail.com>
2026-04-23 17:35:30 -0300

    Update help.js

--- src/utils/help.js
+++ src/utils/help.js
@@ -1,10 +1,44 @@
 import { COMMANDS_MAP } from './constants';
 
 export const formatHelpMessage = (dbUserGroups = []) => {
-  let response = '\n Current availables commands: \n';
-  
-  const availablesCommands = Object.values(COMMANDS_MAP);
-  availablesCommands.forEach(({ groups, howToUse }) => {
+
+  let response = `
+
+[b]⚔️ SPK TEAM - COMANDOS DISPONÍVEIS ⚔️[/b]
+
+[b]📌 UTILITÁRIOS[/b]
+!desc - Link para gerar sua descrição
+!loot <log> - Divide loot da hunt
+!lk <nome> - Lista os matadores da última morte
+!spy <nome> - Ver possíveis personagens secundários
+!char <nome> - (Em breve) Estatísticas do personagem
+
+`;
+
+  response += `[b]🔐 COMANDOS POR PERMISSÃO[/b]\n`;
+
+  const availableCommands = Object.values(COMMANDS_MAP);
+
+  availableCommands.forEach(({ groups = [], howToUse }) => {
+
+    if (!howToUse) return;
+
+    // ignora comandos que você não quer mais mostrar
+    const hiddenCommands = [
+      '!addNeutral',
+      '!removeNeutral',
+      '!addMakersEnemy',
+      '!removeMakersEnemy',
+      '!addMakersFriend',
+      '!removeMakersFriend',
+      '!addPossibleEnemys',
+      '!removePossibleEnemys'
+    ];
+
+    const commandName = howToUse.split(' ')[0];
+
+    if (hiddenCommands.includes(commandName)) return;
+
     let isVisibleByGroup = false;
 
     dbUserGroups.forEach(({ name }) => {
@@ -13,10 +47,13 @@ export const formatHelpMessage = (dbUserGroups = []) => {
       }
     });
 
-    if (howToUse && isVisibleByGroup) {
-      response += `${howToUse} \n`;
+    if (groups.length === 0 || isVisibleByGroup) {
+      response += `${howToUse}\n`;
     }
+
   });
+
+  response += `\n🌐 Portal: https://spkteam.duckdns.org\n`;
 
   return response;
 };
