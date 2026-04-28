5afca75810764130ecc25fa26e6973ea0c4ff0ee
Ediin <uai.coins@gmail.com>
2026-04-28 13:29:23 -0300

    Update lists.js

--- src/api/crone/lists.js
+++ src/api/crone/lists.js
@@ -585,7 +585,7 @@ export const startTasks = (teamspeak) => {
 
   }, { scheduled: false });
 
-  const neutralTask = cron.schedule('*/5 * * * *', async () => {
+  const neutralTask = cron.schedule('0 * * * * *', async () => {
     if (isSlowTaskRunning) {
       console.log('[CRON] neutralTask ainda em execução. Pulando esta rodada.');
       return;
